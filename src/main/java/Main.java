5f95961686b62f1bb951abc1e416b5b34de6882f
amehlhas <a.mehlhase@asu.edu>
2025-10-10 14:34:40 -0700

    Add encouraging message for players

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Welcome to the Number Guessing Game! ===");
+        System.out.println("Good luck!");
 
         GameEngine engine = new GameEngine(1, 100);
         GameUI ui = new GameUI(engine, new Scanner(System.in));
